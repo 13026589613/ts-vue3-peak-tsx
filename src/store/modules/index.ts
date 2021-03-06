bce2593cfefa9aed23b2e4bef075afe32b74d475
shaopeng <283373648@qq.com>
2021-03-07 02:21:04 +0800

    初始化代码结构

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -4,43 +4,11 @@
  *              采用注入式，store 会自动检测并填充 state、modules、getters。注入后可采用 对象化调用或者vue-class 注入式获取值
  * @author PP
  */
-const toHumpClassName = (string: string, formatter: string = '_'): string => {
-  const stringArray = string.split(formatter).filter(str => str && str.trim())
-  stringArray.map((item, index) => {
-    if (index > 0) {
-      stringArray[index] = item.substring(0, 1).toUpperCase() + item.substring(1)
+require
+  .context('./', true, /.ts$/)
+  .keys()
+  .forEach(fileName => {
+    if (fileName.indexOf('index') === -1) {
+      import(`${fileName}`)
     }
   })
-  return stringArray.join('')
-}
-
-const storeModules: any = {}
-const files = require.context('./', true, /.ts$/).keys()
-files.forEach(fileName => {
-  if (fileName.indexOf('index') === -1) {
-    const storeName: string = toHumpClassName(
-      fileName.replace('.', '').replace('/', '').replace('ts', '').replace('.', ''),
-      '-'
-    )
-
-    import(`${fileName}`)
-  }
-})
-
-// const path = require('path')
-// const requireModules = require.context('./normal', true, /index\.(ts|js)$/iu)
-// console.log(requireModules.keys())
-
-// const modules: any = {}
-// requireModules.keys().forEach((filePath: string): void => {
-//   const modular = requireModules(filePath)
-
-//   let name = path.resolve(filePath, '..')
-//   name = name.split('/').pop()
-//   modules[name] = {
-//     namespaced: true,
-//     ...modular.default,
-//   }
-// })
-
-// console.log(modules)
